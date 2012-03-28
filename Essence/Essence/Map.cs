5c2b98e10f57211d11668fb53a4629cdc93b13eb
Alex Gilday <alex.gilday@gmail.com>
2012-03-28 20:04:33 +0100

    Implementing new binary file format for maps, can still be loaded from images but not optional. Bug in binary loaded maps, data seems to be offset by one in both directions and I don't know the reason. Code is pretty much identical in saving and loading

--- Essence/Essence/Map.cs
+++ Essence/Essence/Map.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System.IO;
 
 namespace Essence {
 
@@ -25,25 +26,58 @@ namespace Essence {
 		public Map(String s, bool canCameraOverflow, Essence essence) {
 			game = essence;
 			CameraOverflow = canCameraOverflow;
+			LoadMap(s);
+			game.TheWorld.maps.Add(this);
+		}
 
-			Texture2D map = game.Content.Load<Texture2D>(s);
+		public void LoadMapFromImage(String fileName) {
+			Texture2D map = game.Content.Load<Texture2D>(fileName);
 			Color[] colours = new Color[map.Width * map.Height];
 
 			map.GetData(colours);
 			Data = new byte[map.Width, map.Height];
 
 			for(int i = 0; i < map.Width; i++) for(int j = 0; j < map.Height; j++) {
-					byte id = 0;
-					foreach(Terrain t in Terrain.terrainList)
-						if(t != null && t.readCol.Equals(colours[i + j * map.Width])) {
-							id = t.terrainID;
-							break;
-						}
-					
-					Data[i, j] = id;
-				}
+				byte id = 0;
+				foreach(Terrain t in Terrain.terrainList)
+					if(t != null && t.readCol.Equals(colours[i + j * map.Width])) {
+						id = t.terrainID;
+						break;
+					}
 
-			game.TheWorld.maps.Add(this);
+				Data[i, j] = id;
+			}
+		}
+
+		public void SaveMap(String fileName) {
+			int w = Data.GetUpperBound(0);
+			int h = Data.GetUpperBound(1);
+			byte[] binaryData = new byte[w * h + 4];
+			binaryData[0] = (byte) (w / 256);
+			binaryData[1] = (byte) (w % 256);
+			binaryData[2] = (byte) (h / 256);
+			binaryData[3] = (byte) (h % 256);
+
+			for(int i = 0; i < w; i++) for(int j = 0; j < h; j++)
+				binaryData[i + j * w + 4] = Data[i, j];
+
+			FileStream fs = File.Create(fileName, binaryData.Length, FileOptions.None);
+			BinaryWriter bw = new BinaryWriter(fs);
+			bw.Write(binaryData);
+			bw.Close();
+			fs.Close();
+		}
+
+		public void LoadMap(String fileName) {
+			BinaryReader b = new BinaryReader(File.Open(fileName, FileMode.Open));
+			int length = (int) b.BaseStream.Length;
+			byte[] binaryData = b.ReadBytes(length); ;
+			int w = binaryData[0] * 256 + binaryData[1];
+			int h = binaryData[2] * 256 + binaryData[3];
+			Data = new byte[w, h];
+
+			for(int i = 0; i < w; i++) for(int j = 0; j < h; j++)
+				Data[i, j] = binaryData[i + j * w + 4];
 		}
 	}
 }
--- Essence/Essence/World.cs
+++ Essence/Essence/World.cs
@@ -18,7 +18,7 @@ namespace Essence {
 		public void LoadWorld() {
 			grassTex = game.Content.Load<Texture2D>("grass");
 			foreach(Terrain t in Terrain.terrainList) if(t != null) t.Load();
-			maps.Add(new Map("map", game));
+			maps.Add(new Map("map1.bin", game));
 		}
 
 		public void Draw(GameTime gt) {
